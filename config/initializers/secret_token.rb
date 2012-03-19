d414f99fb88a638d086f44578cabfe85545a05e5
Sandro <sandro@pecchia.info>
2012-03-20 00:01:33 +0100

    base con rails 321 e ruby 193.0p

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fispo::Application.config.secret_token = '2b5b2c7dd25eb8e36da6535e4f92aa93278f32da25b1e645d57584479b0a6a7fa150b7f2076bf25d0486a3483afab01b4fb04100a2d2e165892e8d316168f044'
