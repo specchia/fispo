217be2ee083cb6eb39afbab8d29a5d23ebd71b0f
THUAL Dominique <dom_thual@yahoo.fr>
2012-04-16 22:39:27 +0200

    Add session Helper for Authentication and roles functions
    Add some routine to define Master with roles
    To improve and integrate Devise helpers
    
    Add an exmaple for html with url definition server side
    
    How to use roles:  u = Account.new
     role assignment     u.roles = [:admin]  u.roles = ['admin']
     adding roles (remove via delete or re-assign)       u.roles << :manager
     querying roles...
     get all valid roles that have been declared   Account.valid_roles  => [:admin, :manager, :collaboratore, :abbonato]
     ... retrieve all assigned roles  u.roles  => [:admin, :manager]
     ... check for individual roles   u.has_role? :author  => false       u.is? :admin  => true
     ... check for multiple roles     u.has_any_role? :author, :manager   u.is_any_of? :author, :manager  => true
                                      u.has_all_roles? :author, :manager  u.is? :author, :manager  => false

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,63 @@
+module SessionsHelper
+
+#TODO: Verificare se cambiare tutti current_user => current_account!
+
+#PROTECT PAGE using before_filter :authenticate, :only => [:edit, :update]
+#  def sign_in(user)
+#    user.remember_me!
+#    cookies[:remember_token] = { :value   => user.remember_token, :expires => 20.years.from_now.utc }
+#    self.current_user = user
+#  def current_user=(user)
+#    @current_user = user
+#  def current_user
+#    @current_user ||= user_from_remember_token #||= (“or equals”) assignment operator only if @current_user is undefined
+#  def user_from_remember_token
+#    remember_token = cookies[:remember_token]
+#    User.find_by_remember_token(remember_token) unless remember_token.nil?
+#  def signed_in?
+#    !current_user.nil?
+#  def sign_out
+#    cookies.delete(:remember_token)
+#    self.current_user = nil
+#  def current_user?(user)
+#    user == current_user
+
+  def signed_in_and_master?
+    signed_in? && (current_user.name == 'dom'  ||  current_user.name == 'specchia')
+    signed_in? && (current_user.is? :admin)
+  end
+
+  def get_fisco_user
+    if signed_in_and_master?
+        User.find(1)
+    else
+        User.find(:first, :conditions => [ "user_id = ?", current_user.id])
+    end
+  end
+
+  def deny_access_destroy
+    flash[:notice] = "Sorry. Only technical manager can delete data. Please, contact Roberto SPURIO to do it."
+    redirect_to request.request_uri
+  end
+  def deny_access
+    store_location  #Friendly forwarding
+    flash[:notice] = "Please sign in to access this page."
+    redirect_to signin_path
+  end
+  #Friendly forwarding
+  def store_location
+    #puts the requested URL in the session variable under the key :return_to
+    session[:return_to] = request.request_uri
+  end
+  #redirect to the requested URL if it exists, or some default URL otherwise.
+  #This method is needed in the Session create action to redirect after successful signin
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    #clear_return_to
+  end
+  def clear_return_to
+    session[:return_to] = nil
+  end
+
+end
+
--- app/models/section.rb
+++ app/models/section.rb
@@ -2,7 +2,20 @@ class Section < ActiveRecord::Base
   has_many :contents
   belongs_to :top_section,  :foreign_key => "top_section_id"
 
+  #include ActionController::UrlWriter   #Rails 2 
+  include Rails.application.routes.url_helpers   #Rails 3
+
   def section_with_id
     "#{id} . #{sezione}"
   end
+
+  def edit
+      #include ActionController::UrlWriter 
+      #if auth_sample_user(self.id) or signed_in_and_master?
+      "<a href='" + edit_sampling_path(self) + "' title='Edit selected row'><div class='ui-pg-div' title='Edit selected row'><span class='ui-icon ui-icon-pencil' title='Edit selected row'></span></div></a>"
+      #else
+      #    ""
+      #end
+  end
+
 end
