df1e996a26550d10c0828b4c7aafce31084d00c4
THUAL Dominique <dom_thual@yahoo.fr>
2012-04-18 19:45:51 +0200

    cancan see_timestamp

---
+++ app/models/ability.rb
@@ -0,0 +1,42 @@
+class Ability
+  include CanCan::Ability
+ 
+  def initialize(account)
+    account ||= Account.new # guest user
+
+    #The level of authorizations  :create, :read, :update, :delete, :manage
+
+    #roles :admin, :manager, :collaboratore, :abbonato 
+    if account.role? :admin
+      can :manage, :all   #[Role, ]
+      can :see_timestamps, Account
+    elsif account.role? :manager
+      can :read, :all
+      can :manage, [Account, User, Content, Section, TopSection]
+      can :see_timestamps, Account
+    elsif account.role? :collaboratore
+      can :read, :all
+      can :see_timestamps, Account, :id => account.id      
+      can :manage, [Account, User, Content]
+    elsif account.role? :abbonato
+      can :read, :all     #can access the index or show action of either of those models
+      can :see_timestamps, Account, :id => account.id      
+      # manage products, assets he owns
+      can :manage, Content do |content|
+        content.try(:owner) == account
+      end
+#      can :manage, Asset do |asset|
+#        asset.assetable.try(:owner) == account
+#      end
+    else
+      #can :read, :all
+      can :read, [Content]
+      can :see_timestamps, Account, :id => account.id      
+    end
+
+  end
+end
+
+#Adding authorization
+#Finally, in every controller that needs authorization, we add this line:
+#load_and_authorize_resource
